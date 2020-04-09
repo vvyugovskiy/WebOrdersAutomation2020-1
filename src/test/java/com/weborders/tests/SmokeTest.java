cdf39796e6272303c42560d81a60edd1a313a5e1
Vasyl Fomiuk <vasyl@cybertekschool.com>
2020-04-08 21:25:54 -0400

    updated driver class to run in parallel

--- src/test/java/com/weborders/tests/SmokeTest.java
+++ src/test/java/com/weborders/tests/SmokeTest.java
@@ -21,7 +21,7 @@ public class SmokeTest extends AbstractBaseTest {
         extentTest.pass(component + " verified!");
     }
 
-    @DataProvider
+    @DataProvider(parallel = true)
     public Object[][] smokeTestData() {
         return new Object[][]{
                 {"View all orders", "List of All Orders"},
--- src/test/java/com/weborders/utilities/Driver.java
+++ src/test/java/com/weborders/utilities/Driver.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Driver {
 
     //same for everyone
-    private static WebDriver driver;
+    private static ThreadLocal<WebDriver> driverPool = new ThreadLocal<>();
 
     //so no one can create object of Driver class
     //everyone should call static getter method instead
@@ -20,7 +20,7 @@ public class Driver {
     public static WebDriver getDriver() {
         //if webdriver object doesn't exist
         //create it
-        if (driver == null) {
+        if (driverPool.get() == null) {
             //specify browser type in configuration.properties file
             String browser = ConfigurationReader.getProperty("browser").toLowerCase();
             switch (browser) {
@@ -28,30 +28,30 @@ public class Driver {
                     WebDriverManager.chromedriver().version("79").setup();
                     ChromeOptions chromeOptions = new ChromeOptions();
                     chromeOptions.addArguments("--start-maximized");
-                    driver = new ChromeDriver(chromeOptions);
+                    driverPool.set(new ChromeDriver(chromeOptions));
                     break;
                 case "chromeheadless":
                     //to run chrome without interface (headless mode)
                     WebDriverManager.chromedriver().version("79").setup();
                     ChromeOptions options = new ChromeOptions();
                     options.setHeadless(true);
-                    driver = new ChromeDriver(options);
+                    driverPool.set(new ChromeDriver(options));
                     break;
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
+                    driverPool.set(new FirefoxDriver());
                     break;
                 default:
                     throw new RuntimeException("Wrong browser name!");
             }
         }
-        return driver;
+        return driverPool.get();
     }
 
     public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
+        if (driverPool != null) {
+            driverPool.get().quit();
+            driverPool.remove();
         }
     }
 }
